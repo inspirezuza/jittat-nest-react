27a5ad31a0e4966121ce985f1cc2738b9a623cc7
inspirezuza <inspire7220@gmail.com>
2024-07-05 12:46:10 +0700

    returns message from backend, shows message in frontend

---
+++ cr-backend/src/courses.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get('hello')
+  findAll(): any {
+    return {
+      message: 'Hello from courses controller!',
+    };
+  }
+}
