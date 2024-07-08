79e5f21472bca583867001a8f7dd415a3f31674e
inspirezuza <inspire7220@gmail.com>
2024-07-09 01:38:42 +0700

    show courses

--- cr-backend/src/courses.controller.ts
+++ cr-backend/src/courses.controller.ts
@@ -2,10 +2,12 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get('hello')
+  @Get()
   findAll(): any {
-    return {
-      message: 'Hello from courses controller!',
-    };
+    return [
+      { id: '100', title: 'Computer and Programming1', number: '01204111' },
+      { id: '200', title: 'Computer and Programming2', number: '01204112' },
+      { id: '300', title: 'Computer and Programming3', number: '01204113' },
+    ];
   }
 }
--- cr-frontend/src/App.tsx
+++ cr-frontend/src/App.tsx
@@ -3,14 +3,25 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [message, setMessage] = useState("My message");
+  const [courses, setCourses] = useState<any[]>([{ title: "Default" }]);
   useEffect(() => {
-    fetch("http://localhost:3000/courses/hello")
+    fetch("http://localhost:3000/courses")
       .then((res) => res.json())
-      .then((obj) => setMessage(obj.message));
+      .then((courses) => {
+        setCourses(courses);
+      });
   }, []);
-
-  return <div>{message}</div>;
+  return (
+    <div>
+      <ul>
+        {courses.map((item) => (
+          <li key={item.id}>
+            {item.number} - {item?.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
